7d6dc600d70e958d714da697fd32ca31c5d6c576
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2015-11-14 01:13:41 +0100

    Set SQLALCHEMY_TRACK_MODIFICATIONS to False
    SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be
    disabled by default in the future.

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard7ogu3sS5tring'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     APP_ADMIN = os.environ.get('APP_ADMIN')
     @staticmethod
     def init_app(app):
